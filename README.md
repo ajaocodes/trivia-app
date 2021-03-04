0c97edc1c6aff410d5da469af994e32bb479d8fb
omololu ajao <ajaoomololu@gmail.com>
2021-03-03 19:07:55 -0800

    API functioning properly, questions and answers

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # SPORT THE ANSWER
 
-This is a trivia game that will test how well rounded you are about **SPORTS**. This game will teach you new information and test what you know about various athletes, legends of the game, rules of the sports and the history of sports.  
+This is a trivia game that will test how well rounded you are about **SPORTS** from all over the world. This game will teach you new information and test what you know about various athletes, legends of various sports, rules of various sports and history of various sports.  
 
- The question type will be multiple choice and medium difficulty.
+ The five questions asked will be multiple choice with medium difficulty.
 
 
  ## Technologies used: 
--- js/script.js
+++ js/script.js
@@ -1,9 +1,109 @@
 //Variable and constants
 
-
-
+//API URL
 const url = "https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple"
+const gameStart = {
+    player1: 0,
+    player2: 0,
+    currentquestion: {},
+}
+
+// let questions = [];
+const $upper = $('#upper')
+const $question = $('#question')
+const $answer = $('#answer')
+const $a = $('#a') 
+const $b = $('#b') 
+const $c = $('#c') 
+const $d = $('#d') 
+const $scores = $('#scores') 
+const $player1 = $('#player1 h4') 
+const $player2 = $('#player2 h4') 
+
+
+//*************************************/
+//FUNCTIONS
+//*********************************** */
+
+const setBoard = (question) => {
+    const randomIndex = Math.floor(Math.random() * question.length)
+    const randomQuestion = question[randomIndex]
+    
+    const allAnswers = [
+                     randomQuestion.correct_answer,
+                     randomQuestion.incorrect_answers[0],
+                     randomQuestion.incorrect_answers[1],
+                     randomQuestion.incorrect_answers[2]
+    ]
+
+    let randomAns;
+    const answerSpots = [$a,$b, $c, $d]
+    allAnswers.forEach((answer) => {
+        randomAns = Math.floor(Math.random() * answerSpots.length)
+        answerSpots[randomAns].text(answer)
+        answerSpots.splice(randomAns, 1)
+      })
+    
+    //Updating questions 
+$question.text(randomQuestion.question) 
+
+//Updating Player scores
+$player1.text(gameStart.player1)
+$player2.text(gameStart.player2)
+
+
+//event listeners
+
+const chooseAnswer = (event, question) => {
+    // console.log(event)
+    if (event.target.innerText === randomQuestion.correct_answer)  {
+        console.log("correct")
+        setBoard(questions)
+    } else {
+        // setBoard(questions)
+        console.log("incorrect")
+    }
+}
+
+  $('li').on("click", (event) => {
+    chooseAnswer(event, randomQuestion)
+})
+
+}
+
+
 $.ajax(url)
   .then((data) => {
-    console.log(data)
+
+    //   console.log(data.results)
+      questions = data.results;
+   setBoard(questions)
+   console.log(questions)
+    // console.log(setBoard(questions))
 })
+
+
+// console.log(questions)
+// console.log(data)
+
+
+
+
+
+// $('li').ready(function(){
+//     $("li").on({
+    
+//          mouseenter: function(){
+//             $("li").css("background-color", "gray");
+//         },  
+        
+//         mouseleave: function(){
+//             $("li").css("background-color", "white");
+//         }, 
+        
+//         dblclick: function(){
+//             $("li").css("background-color", "yellow");
+//         },
+       
+//     });
+// });
